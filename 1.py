3fce61de309fc14deef8a0322f9640be16b62ff2
t.vladimir <vovatatosyan2000@mail.ru>
2021-04-11 22:40:43 +0300

    First

---
+++ 1.py
@@ -0,0 +1,29 @@
+import sys
+#import pandas
+
+
+class MyClass:
+    i = 12345
+
+    def f(self):
+        return 'hello world'
+
+def foo():
+    a = 1
+    b = MyClass()
+    c = [1, 2, 3]
+    #d = pandas.read_csv("my_file.csv")
+    print_vars()
+
+def print_vars():
+    frame = sys._getframe()
+    locals = frame.f_back.f_locals
+    print(locals.keys())
+    for el in locals.keys():
+        value = locals.get(el)
+        if value.__class__.__module__ == '__builtin__':
+            print(el + ':' + ' True')
+        else:
+            print(el + ':' + ' False')
+    
+foo()
